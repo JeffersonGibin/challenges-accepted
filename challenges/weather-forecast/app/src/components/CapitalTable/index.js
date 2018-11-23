c0c79de85010e20cf3415de4bc027e502aa825ab
AndersonMamede <avatar04@hotmail.com>
2018-11-23 17:48:59 -0200

    Split Capitals table into columns

--- challenges/weather-forecast/app/src/components/CapitalTable/index.js
+++ challenges/weather-forecast/app/src/components/CapitalTable/index.js
@@ -3,23 +3,34 @@ import CapitalRow from './../CapitalRow/';
 
 class CapitalTable extends React.Component {
     render() {
+        let capitalsChunks = [
+            this.props.capitals.slice(0, this.props.capitals.length/2),
+            this.props.capitals.slice(this.props.capitals.length/2)
+        ];
+        
         return (
             <section className="capitals">
                 <h3 className="capitals__title">Capitals</h3>
-                <table className="capitals__table">
-                    <thead>
-                        <tr>
-                            <th>Min</th>
-                            <th>Max</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.capitals.map(capital => 
-                            <CapitalRow key={capital.key} capital={capital} />
-                        )}
-                    </tbody>
-                </table>
+                <div>
+                    {capitalsChunks.map((capitals, key) =>
+                        <div key={key} className="capitals__column-wrapper">
+                            <table className="capitals__table">
+                                <thead>
+                                    <tr>
+                                        <th>Min</th>
+                                        <th>Max</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {capitals.map(capital => 
+                                        <CapitalRow key={capital.key} capital={capital} />
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
+                </div>
             </section>
         );
     }
