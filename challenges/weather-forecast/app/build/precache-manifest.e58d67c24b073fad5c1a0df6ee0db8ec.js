895f9cc8a03a4dc45405f78d03a409a6afbc3432
AndersonMamede <avatar04@hotmail.com>
2018-12-10 17:56:15 -0200

    Build

---
+++ challenges/weather-forecast/app/build/precache-manifest.e58d67c24b073fad5c1a0df6ee0db8ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "87a42f035e27bbc787e87c1862be2d47",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/media/icon-search.87a42f03.svg"
+  },
+  {
+    "revision": "4c0f8eddea90ab7d4c74bc6499002875",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/media/icon-low.4c0f8edd.svg"
+  },
+  {
+    "revision": "2191c1b55c7e04b26535e91e9ce94c81",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/media/icon-high.2191c1b5.svg"
+  },
+  {
+    "revision": "e22f63848c0a54e23bdb0a4fb0501caa",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/media/icon-heart.e22f6384.svg"
+  },
+  {
+    "revision": "59368f90f9b0ca882217c94479ead971",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/media/favicon.59368f90.ico"
+  },
+  {
+    "revision": "45d706fa0b61a0300fc1",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/js/runtime~main.45d706fa.js"
+  },
+  {
+    "revision": "f8def72d652bacd487a3",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/js/main.f8def72d.chunk.js"
+  },
+  {
+    "revision": "74fa59decb2b62520ab6",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/js/1.74fa59de.chunk.js"
+  },
+  {
+    "revision": "f8def72d652bacd487a3",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/static/css/main.f73bc09a.chunk.css"
+  },
+  {
+    "revision": "dde38527248442379f951ebf57beedc9",
+    "url": "/challenges-accepted/challenges/weather-forecast/app/build/index.html"
+  }
+];
--- challenges/weather-forecast/app/build/service-worker.js
+++ challenges/weather-forecast/app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.34bc83f53e258965cf8defb33d2a2f28.js"
+  "/challenges-accepted/challenges/weather-forecast/app/build/precache-manifest.e58d67c24b073fad5c1a0df6ee0db8ec.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/challenges-accepted/challenges/weather-forecast/app/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
