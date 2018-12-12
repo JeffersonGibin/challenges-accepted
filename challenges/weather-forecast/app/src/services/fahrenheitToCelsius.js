0ba2ad9c1b16197448c6985b809560df02f31328
Jefferson <jeffersongibin@yahoo.com.br>
2018-12-11 23:26:59 -0200

    Adaptações na estrutura após o fork

--- challenges/weather-forecast/app/src/services/fahrenheitToCelsius.js
+++
@@ -1,5 +0,0 @@
-function fahrenheitToCelsius(temperature) {
-    return Math.round((temperature - 32) * (5/9))
-}
-
-export default fahrenheitToCelsius;
--- challenges/weather-forecast/app/src/services/splitList.js
+++
@@ -1,9 +0,0 @@
-function splitList(list) {
-    list = Array.isArray(list) ? list : [];
-    return {
-        firstChunk: list.slice(0, list.length/2),
-        secondChunk: list.slice(list.length/2),
-    };
-}
-
-export default splitList;
